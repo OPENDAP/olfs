ee9c9eb3a6aabeb8a1f1bd86811d2bb9cd38e58a
ndp <ndp@4ba5f37b-aaf8-0310-9c43-92a51b5764a6>
2007-11-14 18:22:34 +0000

    olfs: Updated Version to 1.3.0

--- src/opendap/bes/Version.java
+++ src/opendap/bes/Version.java
@@ -39,8 +39,8 @@ import opendap.coreServlet.ReqInfo;
 public class Version  {
 
 
-    private static String olfsVersion  = "1.2.3";
-    private static String hyraxVersion = "1.2.1";
+    private static String olfsVersion  = "1.3.0";
+    private static String hyraxVersion = "1.3.0";
 
 
 
