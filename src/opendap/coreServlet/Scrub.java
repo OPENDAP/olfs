3525569bb56a7fd51cd5ab692301a118683e9d70
ndp <ndp@4ba5f37b-aaf8-0310-9c43-92a51b5764a6>
2008-02-28 19:37:35 +0000

    olfs: Added JUnit tests for Scrub.java

--- src/opendap/coreServlet/Scrub.java
+++ src/opendap/coreServlet/Scrub.java
@@ -26,6 +26,9 @@ package opendap.coreServlet;
 
 import org.slf4j.Logger;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -99,8 +102,8 @@ public class Scrub {
 
 
 
-    private static String simpleStringInclusionRegex = "[a-zA-Z0-9]*";
-    private static String simpleStringExclusionRegex = "[^a-zA-Z0-9]";
+    private static String simpleStringInclusionRegex = "[a-zA-Z0-9_ ]*";
+    private static String simpleStringExclusionRegex = "[^a-zA-Z0-9_ ]";
 
     private static Pattern simpleStringInclusionPattern = Pattern.compile(simpleStringInclusionRegex);
 
@@ -133,24 +136,92 @@ public class Scrub {
 
 
     public static void main(String[] args){
+        org.junit.runner.JUnitCore.main("opendap.coreServlet.Scrub");
+    }
 
 
-        String s = "this <> should suck.";
+    @org.junit.Test public void test() {
 
-        System.out.println("source: "+s+"   result: "+urlContent(s));
 
-        s = "this IsGood";
+        checkURLContent("This <> should suck.", false);
 
-        System.out.println("source: "+s+"   result: "+urlContent(s));
 
+        checkURLContent("this IsGood", true);
+
+
+
+        checkCompleteURL("http://.../OAK_00168/microstar227926-20080205.nc",
+                true);
+
+        checkCompleteURL("http://dods.jpl.nasa.gov/opendap/" +
+                "sea_surface_temperature/" +
+                "avhrr/pathfinder/data_v4.1/best_sst/monthly/ascending/09km/" +
+                "1987/198704h09ma-gdm.hdf.Z.dds",
+                true);
+
+        checkCompleteURL("http://dods.jpl.nasa.gov/opendap/ocean_wind/ssmi/" +
+                "msfc_pathfinder/level3/data/pentad/contents.html",
+                true);
+
+        checkCompleteURL("http://cdaweb.gsfc.nasa.gov/cgi-bin/opendap/" +
+                "nph-dods/" +
+                "istp_public/data/isis1/ott/1975/184/" +
+                "i1_av_ott_1975184014529_v01.cdf.html",
+                true);
+
+        checkCompleteURL("http://ingrid.ldeo.columbia.edu/SOURCES/.OORT/" +
+                "dods.dds",
+                true);
+
+        checkCompleteURL("http://dods.mbari.org/cgi-bin/nph-nc/data/oasis/" +
+                "netcdf/" +
+                "dailyAveragedM2.nc.html",
+                true);
+
+        checkCompleteURL("http://dataportal.ucar.edu:9191/dods/" +
+                "ASSIMtF_COLAMOM3/Tot/" +
+                "current.dds",
+                true);
+
+
+    }
+
+    private static void checkCompleteURL(String s, boolean expected){
+
+        simpleCheck(s,completeURL(s),expected);
 
 
     }
 
 
+    private static void checkURLContent(String s, boolean expected){
+
+
+        simpleCheck(s,urlContent(s),expected);
+
+
+    }
+
+    private static void simpleCheck(String s1, String s2, boolean expected){
+
+        boolean value =  s1.equals(s2);
+
+        if(expected){
+            String msg = value?"PASS (Strings Match):  ":"FAIL (Strings DO NOT Match):  ";
+            msg+= "s1=\""+s1+"\";   s2: "+s2;
+            //System.out.println(msg);
+            assertTrue(msg,value);
+        }
+        else{
+            String msg = value?"FAIL (Strings Match):  ":"PASS (Strings DO NOT Match):  ";
+            msg+= "s1=\""+s1+"\";   s2: "+s2;
+            //System.out.println(msg);
+            assertFalse(msg,value);
+        }
 
 
 
+    }
 
 
 
