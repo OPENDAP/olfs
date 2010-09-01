7a67e3d70a506461763d48e6f29b0a9f5759751d
haibo <haibo@4ba5f37b-aaf8-0310-9c43-92a51b5764a6>
2010-09-01 15:45:39 +0000

    add IRIHTTPRepository.java

---
+++ src/opendap/semantics/IRISail/IRIHTTPRepository.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package opendap.semantics.IRISail;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.http.HTTPRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author haibo
+ *
+ */
+
+    public class IRIHTTPRepository extends HTTPRepository {
+        private static Logger log ;
+
+
+        private AtomicBoolean isRepositoryDown;
+
+        public IRIHTTPRepository(String serverURL, String repositoryID) {
+
+            super(serverURL, repositoryID);
+
+            log = LoggerFactory.getLogger(getClass());
+            isRepositoryDown = new AtomicBoolean();
+        }
+        public void initialize() throws org.openrdf.repository.RepositoryException {
+            super.initialize();
+            isRepositoryDown.set(false);
+        }
+
+        public Boolean isRepositoryDown() {
+            return isRepositoryDown.get();
+        }
+
+        /**
+         * Does a repository shutdown, but only if the repository is running!.
+         *
+         * @throws RepositoryException
+         */
+        @Override
+        public void shutDown() throws RepositoryException {
+
+            log.debug("shutDown(): Shutting down Repository...");
+            if (!isRepositoryDown.get()) {
+                super.shutDown();
+                isRepositoryDown.set(false);
+                log.info("shutDown(): Semantic Repository Has Been Shutdown.");
+            } else {
+                log.info("shutDown(): Semantic Repository was already down.");
+            }
+            log.debug("shutDown(): Repository shutdown complete.");
+        }
+
+}
