4d119336d5a95ecd074e47d4db2626a80e80be6d
haibo <haibo@4ba5f37b-aaf8-0310-9c43-92a51b5764a6>
2010-09-10 20:24:17 +0000

    documents

--- src/opendap/semantics/IRISail/ProcessingState.java
+++ src/opendap/semantics/IRISail/ProcessingState.java
@@ -1,3 +1,28 @@
+/////////////////////////////////////////////////////////////////////////////
+// This file is part of the "OPeNDAP Web Coverage Service Project."
+//
+// Copyright (c) 2010 OPeNDAP, Inc.
+//
+// Authors:
+//     Nathan David Potter  <ndp@opendap.org>
+//     
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
+/////////////////////////////////////////////////////////////////////////////
 package opendap.semantics.IRISail;
 
 import org.slf4j.Logger;
@@ -6,11 +31,8 @@ import org.slf4j.LoggerFactory;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
- * Created by IntelliJ IDEA.
- * User: ndp
- * Date: Aug 26, 2010
- * Time: 5:44:14 PM
- * To change this template use File | Settings | File Templates.
+ * This class is used to track the state of a thread updating the repository.
+ * It makes sure that the clients get current information from the repository.
  */
 public class ProcessingState {
 
