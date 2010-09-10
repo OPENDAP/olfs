5bab1e9db0e4742eb078f6c24c0513b51939b78c
haibo <haibo@4ba5f37b-aaf8-0310-9c43-92a51b5764a6>
2010-09-10 15:34:02 +0000

    documents

--- src/opendap/semantics/IRISail/XMLfromRDF.java
+++ src/opendap/semantics/IRISail/XMLfromRDF.java
@@ -1,6 +1,29 @@
-/**
- * 
- */
+/////////////////////////////////////////////////////////////////////////////
+// This file is part of the "OPeNDAP Web Coverage Service Project."
+//
+// Copyright (c) 2010 OPeNDAP, Inc.
+//
+// Authors:
+//     Haibo Liu  <haibo@iri.columbia.edu>
+//     Nathan David Potter  <ndp@opendap.org>
+//     M. Benno Blumenthal <benno@iri.columbia.edu>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
+/////////////////////////////////////////////////////////////////////////////
 package opendap.semantics.IRISail;
 
 import java.io.IOException;
@@ -29,12 +52,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Create a JDOM document by querying against Sesame-OWLIM RDF store. The rootElementStr
- * is the outer wrapper of the document. The topURI is the top element to retrieve
+ * This class is used to create a JDOM document by querying against Sesame-OWLIM RDF store.
+ * The <code>rootElementStr</code>
+ * is the outer wrapper of the document. The <code>topURI</code> is the top element to retrieve
  * from the repository.
  *
- * @author Haibo 
- * @version 1.0
  */
 public class XMLfromRDF {
 	private RepositoryConnection con;
