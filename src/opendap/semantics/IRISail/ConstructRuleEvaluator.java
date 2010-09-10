3a366626ecf680a3da44e7291c078f1a166f6476
haibo <haibo@4ba5f37b-aaf8-0310-9c43-92a51b5764a6>
2010-09-10 15:21:38 +0000

    documents

--- src/opendap/semantics/IRISail/ConstructRuleEvaluator.java
+++ src/opendap/semantics/IRISail/ConstructRuleEvaluator.java
@@ -1,3 +1,30 @@
+
+ /////////////////////////////////////////////////////////////////////////////
+// This file is part of the "OPeNDAP Web Coverage Service Project."
+//
+// Copyright (c) 2010 OPeNDAP, Inc.
+//
+// Authors:
+//     Haibo Liu  <haibo@iri.columbia.edu>
+//     Nathan David Potter  <ndp@opendap.org>
+//     M. Benno Blumenthal <benno@iri.columbia.edu>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
+/////////////////////////////////////////////////////////////////////////////
 package opendap.semantics.IRISail;
 
 import org.openrdf.model.*;
@@ -17,13 +44,15 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Created by IntelliJ IDEA.
- * User: ndp
- * Date: Aug 26, 2010
- * Time: 12:15:38 PM
- * To change this template use File | Settings | File Templates.
- */
+ /**
+  * This class is used to run external inference rules on an OWLIM-Sesame 2 repository.
+  * The inference rules are written in SeRQL.
+  * The inferred statements are added into the repository. The inference rules are run
+  * repeatedly until no rules generate any new statements.
+  * <p/>
+  * The <code>constructQuery</code> holds the inference rules.
+  * The <code>constructContext</code> holds inference rule and its ID pares.  
+  */
 public class ConstructRuleEvaluator {
 
     private  Logger log;
@@ -988,12 +1017,7 @@ public class ConstructRuleEvaluator {
 
     }
 
-    /**
-     *
-     * @param classInstance
-     * @param methodName
-     * @return
-     */
+    
     public  Method getMethodForFunction(Object classInstance,
             String methodName) {
 
@@ -1027,11 +1051,7 @@ public class ConstructRuleEvaluator {
 
     }
 
-    /**
-     * 
-     * @param m
-     * @return
-     */
+
     public  String getProcessingMethodDescription(Method m) {
 
         String msg = "";
