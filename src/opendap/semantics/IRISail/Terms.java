05c476936741d830b575c675274903139557ef02
benno <benno@4ba5f37b-aaf8-0310-9c43-92a51b5764a6>
2010-09-15 20:03:28 +0000

    added javadoc

--- src/opendap/semantics/IRISail/Terms.java
+++ src/opendap/semantics/IRISail/Terms.java
@@ -29,36 +29,91 @@ package opendap.semantics.IRISail;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * This class holds all terms defined in this package.
+ * This class holds all terms defined in this package.  The strings frequently come
+in pairs, e.g. lastModifiedContext and lastModifiedContextUri, where the first
+the local name, and the second has the namespace prepended to make a
+complete URI.
  */
 public class Terms {
 
     /**
-     * A comment
+     * is the URL of an owl document defining the classes and properties that the persistent RDF cache
+     * uses in its inferencing and queries against the repository. 
+     * It is always a rdfcache:StartingPoint, consequently it is always
+     * included in the repository.
      */
     public static final String internalStartingPoint         = "http://iridl.ldeo.columbia.edu/ontologies/rdfcache.owl";
 
     /**
-     * Another Comment
+     * is the namespace used for the classes and properties used by the persistent RDF cache.
+     * The internalStartingPoint has to include the owl document that defines these classes/properties.
      */
-    public static final String rdfCacheNamespace             = internalStartingPoint+"#";
-
+    public static final String rdfCacheNamespace             = "http://iridl.ldeo.columbia.edu/ontologies/rdfcache.owl#";
+    /**
+     * is the local name of the property used to hold the last_modified time of a document.
+     * The code uses the cacheContext to hold the last_modified time of each document read in
+     */
     public static final String lastModifiedContext           = "last_modified";
+    /**
+     * is the URI of the property used to hold the last_modified time of a document.
+     * The code uses the cacheContext to hold the last_modified time of each document read in
+     */
     public static final String lastModifiedContextUri        = rdfCacheNamespace + lastModifiedContext;
-
+    /**
+     * is the local name of the context used to hold information the code keeps on each document read in.
+     */
     public static final String cacheContext                  = "cachecontext";
+    /**
+     * is the URI of the context used to hold information the code keeps on each document read in.
+     */
     public static final String cacheContextUri               = rdfCacheNamespace + cacheContext;
-
+    /**
+     * is the local name of the property used to hold the content-type of a document.
+     * The code uses the cacheContext to hold the content-type of each document read in.
+     */
     public static final String contentTypeContext            = "Content-Type";
+    /**
+     * is the URI of the property used to hold the content-type of a document.
+     * The code uses the cacheContext to hold the content-type of each document read in.
+     */
     public static final String contentTypeContextUri         = rdfCacheNamespace + contentTypeContext;
-
+    /**
+     * is the local name of the context used to hold the externalInferencing.
+     * The code can be easily changed to use the URI of each rule to hold the rule's output
+     * in it's own context, but deletes were faster with a single context to hold all the
+     * external inferencing.
+     */
     public static final String externalInferencingContext    = "externalInferencing";
+    /**
+     * is the URI of the context used to hold the externalInferencing.
+     * The code can be easily changed to use the URI of each rule to hold the rule's output
+     * in it's own context, but deletes were faster with a single context to hold all the
+     * external inferencing.
+     */
     public static final String externalInferencingContextUri = rdfCacheNamespace + externalInferencingContext;
-
+    /**
+     * is the local name of the context used to hold the {doc} rdf:type {rdfcache:StartingPoint} statements.
+     * There are two classes of documents in the repository:  StartingPoints, and documents needed
+     * (directly or indirectly) by the StartingPoints. This context holds the statements which declare
+     * that the rdfcache:StartingPoint documents are in that class.
+     */
     public static final String startingPointsContext         = "startingPoints";
+    /**
+     * is the URI of the context used to hold the {doc} rdf:type {rdfcache:StartingPoint} statements.
+     * There are two classes of documents in the repository:  StartingPoints, and documents needed
+     * (directly or indirectly) by the StartingPoints. This context holds the statements which declare
+     * that the rdfcache:StartingPoint documents are in that class.
+     */
     public static final String startingPointsContextUri      = rdfCacheNamespace + startingPointsContext;
-
+    /**
+     * is the local name of the Class that holds the StartingPoints, i.e. the documents that are included
+     * in the repository even if they are not needed by another document.
+     */
     public static final String startingPointType             = "StartingPoint";
+    /**
+     * is the URI of the Class that holds the StartingPoints, i.e. the documents that are included
+     * in the repository even if they are not needed by another document.
+     */
     public static final String startingPointContextUri       = rdfCacheNamespace + startingPointType;
 
     public static final String functionsContext              = "myfn";
@@ -94,6 +149,7 @@ public class Terms {
     static {
         localResources = new ConcurrentHashMap<String,String>();
         localResources.put("http://scm.opendap.org/svn/trunk/olfs/resources/WCS/xsl/xsd2owl.xsl", "xsl/xsd2owl.xsl");
+        localResources.put("http://scm.opendap.org/svn/trunk/olfs/resources/WCS/xsl/RDFa2RDFXML.xsl", "xsl/RDFa2RDFXML.xsl");
 
     }
 
