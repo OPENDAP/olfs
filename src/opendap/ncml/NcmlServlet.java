64a21699cf9ed4c1775e92115198f5b6ad82d1e0
ndp <ndp@4ba5f37b-aaf8-0310-9c43-92a51b5764a6>
2010-10-07 19:58:21 +0000

    olfs: Ncml Post service

---
+++ src/opendap/ncml/NcmlServlet.java
@@ -0,0 +1,11 @@
+package opendap.ncml;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ndp
+ * Date: Oct 7, 2010
+ * Time: 3:57:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NcmlServlet {
+}
