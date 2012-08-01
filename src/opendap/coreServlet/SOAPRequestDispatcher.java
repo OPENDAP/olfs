a53c74be6bd55e37b39b1c950f8bc452f4704bf2
ndp <ndp@4ba5f37b-aaf8-0310-9c43-92a51b5764a6>
2012-08-01 21:15:04 +0000

    olfs: Post merge adding new SOAPRequestDispatcher.java class - lost as a casuality of tree conflicts in the merge.

---
+++ src/opendap/coreServlet/SOAPRequestDispatcher.java
@@ -0,0 +1,18 @@
+package opendap.coreServlet;
+
+import org.jdom.Element;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ndp
+ * Date: 7/27/12
+ * Time: 1:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SOAPRequestDispatcher extends NoPostHandler {
+}
