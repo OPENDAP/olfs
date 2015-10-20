77844e54ab639ae052f55c56ac95eabb65775345
Nathan Potter <ndp@opendap.org>
2015-10-20 11:56:35 -0700

    olfs: More comments.

--- src/opendap/io/HyraxStringEncoding.java
+++ src/opendap/io/HyraxStringEncoding.java
@@ -1,11 +1,15 @@
 package opendap.io;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
- * Created by ndp on 10/20/15.
+ * This is a thread safe wrapper of the Hyrax wide String encoding.
+ * The default Charset used is java.nio.charset.StandardCharsets.UTF_8.
+ * This can be changed at run time by utilizing the setCharset() method.
+ *
  */
 public class HyraxStringEncoding {
 
@@ -17,14 +21,14 @@ public class HyraxStringEncoding {
     private static Charset _currentCharSet;
 
     static {
-        _currentCharSet = Charset.forName("UTF-8");
+        _currentCharSet = StandardCharsets.UTF_8;
     }
 
     public static Charset getCharset(){
         Lock lock = _rwLock.readLock();
         try {
             lock.lock();
-            return _currentCharSet;
+            return Charset.forName(_currentCharSet.name());
         }
         finally {
             lock.unlock();
