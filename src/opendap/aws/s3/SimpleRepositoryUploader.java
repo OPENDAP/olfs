7cec258b2b2c36b38a1501e26f0742ba02d1ef20
Nathan Potter <ndp@opendap.org>
2016-09-02 16:07:10 -0700

    Simplified ant targets for S3 upload and repoistory building so that command line args are speciufied in a single ant variable. DOcumentation

--- src/opendap/aws/s3/SimpleRepositoryUploader.java
+++ src/opendap/aws/s3/SimpleRepositoryUploader.java
@@ -33,6 +33,7 @@ public class SimpleRepositoryUploader {
     private String _awsAccessKeyId;
     private String _awsSecretKey;
     private String _repoDirectoy;
+    private boolean _makePubliclyReadable;
 
 
 
@@ -43,14 +44,12 @@ public class SimpleRepositoryUploader {
         Options options = new Options();
 
         options.addOption("h", "help", false, "Usage information.");
-
-        options.addOption("i", "awsId", true, "AWS access key ID for working with S3.");
-        options.addOption("s", "awsKey", true, "AWS secret key for working with S3.");
-
+        options.addOption("p", "public", false, "Makes all of the files uploaded publicly readable");
         options.addOption("v", "verbose", false, "Makes more output...");
         options.addOption("t", "dry-run", false, "Does everything but actually push stuff into S3 ");
 
-
+        options.addOption("i", "awsId", true, "AWS access key ID for working with S3.");
+        options.addOption("s", "awsKey", true, "AWS secret key for working with S3.");
         options.addOption("b", "s3-bucket-name", true, "Name of S3 bucket on which to operate.");
         options.addOption("d", "directory", true, "The fully qualified path to the top directory of " +
                 "the tree you want to ingest into S3..");
@@ -58,7 +57,7 @@ public class SimpleRepositoryUploader {
 
         CommandLine line =   parser.parse(options, args);
 
-        String usage  = this.getClass().getName()+" [-v]  -i AWSAccessKeyID -k AWSSecretKey -n S3BucketName -d repoDirectoryRoot ";
+        String usage  = this.getClass().getSimpleName()+" [-h] [-p] [-v] [-t] -i AWSAccessKeyID -s AWSSecretKey -b S3BucketName -d repoDirectoryRoot ";
 
         StringBuilder errorMessage = new StringBuilder();
 
@@ -73,6 +72,9 @@ public class SimpleRepositoryUploader {
 
         _dryRun = line.hasOption("dry-run");
         System.out.println("Dry-Run mode: "+(_dryRun?"ENABLED":"DISABLED"));
+
+        _makePubliclyReadable = line.hasOption("public");
+        System.out.println("Access: "+(_makePubliclyReadable?"Public":"Private"));
 
         /*
         s3LocalCacheRoot = line.getOptionValue("s3-root");
@@ -103,8 +105,6 @@ public class SimpleRepositoryUploader {
             errorMessage.append("Missing Parameter - You must provide an source directory (with the -d option) from which to build the S3 repo.\n");
         }
 
-
-
         if(errorMessage.length()!=0){
             System.err.println(errorMessage);
             HelpFormatter formatter = new HelpFormatter();
@@ -126,7 +126,7 @@ public class SimpleRepositoryUploader {
             SimpleRepositoryUploader srb = new SimpleRepositoryUploader();
             if(srb.processCommandline(args)){
                 File f = new File(srb._repoDirectoy);
-                srb.s3Ingest(f, "", 0);
+                srb.s3Ingest(f);
             }
         }
         catch (Throwable t){
@@ -137,16 +137,27 @@ public class SimpleRepositoryUploader {
 
 
 
-/*
-<?xml version="1.0" encoding="UTF-8"?>
-<?xml-stylesheet type='text/xsl' href='/opendap.test//index.xsl'?>
-<index xmlns="http://nodc.noaa.gov/s3/catalog/1.0" base="https://s3.amazonaws.com/opendap.test" path="" name="opendap.test" delimiter="/" encoding="UTF-8">
-  <folder name="data" size="231402720" count="1"/>
-</index>
 
 
- */
 
+
+    /**
+     * Builds an index for the passed directory. If the passed File is not a directory or cannot be
+     * This is an example of the index file that will be built.
+     *
+    <?xml version="1.0" encoding="UTF-8"?>
+    <?xml-stylesheet type='text/xsl' href='/opendap.test//index.xsl'?>
+    <index xmlns="http://nodc.noaa.gov/s3/catalog/1.0" base="https://s3.amazonaws.com/opendap.test" path="" name="opendap.test" delimiter="/" encoding="UTF-8">
+      <folder name="data" size="231402720" count="1"/>
+    </index>
+     *
+     *
+     * @param node The directory to index.
+     * @param base The value of the base attribute for this index.
+     * @param parentPath  The index/catalog path to the parent directory.
+     * @return The index document object.
+     * @throws BadIngestException When the passed file is not a directory or is not readable.
+     */
     public Document mkIndexDoc(File node, String base, String parentPath) throws BadIngestException {
 
 
@@ -200,20 +211,57 @@ public class SimpleRepositoryUploader {
     }
 
 
-    public void s3Ingest(File f, String parentPath, int level) throws BadIngestException, IOException {
+    /**
+     * Beginning with {@code startingPoint} this recursive method crawls the filesystem building index files to
+     * represent each directory. These index files along with all of the regular files encountered in the crawl
+     * are uploaded to the S3 bucket identified through the {@code SimpleRepositoryUploader} object's state.
+     * The resulting index.xml files in S3 will provide the same logical view of the regular files as was
+     * manifested by the file system from which they were harvested.
+     *
+     * @param startingPoint The staring pint directry/file for the crawl.
+     * @throws BadIngestException
+     * @throws IOException
+     */
+    public void s3Ingest(File startingPoint) throws IOException, BadIngestException {
+        s3Ingest(startingPoint, "",_makePubliclyReadable,0);
+    }
 
-        String myPath = pathConcat(parentPath , f.getName());
 
-        System.out.println(level + "-----------------------------------------------------------"+level);
-        System.out.println(myPath + " (" + (f.isDirectory()?"dir":"file") + ")");
-        if (f.isDirectory()) {
-            Document index = mkIndexDoc(f,_base,parentPath);
+
+
+    /**
+     * Beginning with {@code startingPoint} this recursive method crawls the filesystem building index files to
+     * represent each directory. These index files along with all of the regular files encountered in the crawl
+     * are uploaded to the S3 bucket identified through the {@code SimpleRepositoryUploader} object's state.
+     * The resulting index.xml files in S3 will provide the same logical view of the regular files as was
+     * manifested by the file system from which they were harvested.
+     *
+     * @param startingPoint The staring pint directry/file for the crawl.
+     * @param parentPath The S3 navigation path tot he parent directory (typically set to "" at the begining of crawl)
+     * @param makePubliclyReadable If true the objects uploaded to S3 will be publicly readable. The default
+     *                             is false/private.
+     * @param level This is a recurisive level counter -
+     * @throws BadIngestException
+     * @throws IOException
+     */
+    public void s3Ingest(File startingPoint, String parentPath, boolean makePubliclyReadable, int level) throws BadIngestException, IOException {
+
+        String myPath = pathConcat(parentPath , startingPoint.getName());
+
+        System.out.println("");
+        System.out.println(level + " ----------------------------------------------------------- "+level);
+        System.out.println("ITEM: " + myPath + " (" + (startingPoint.isDirectory()?"dir":"file") + ")");
+        if (startingPoint.isDirectory()) {
+            System.out.println("- - - - - - - - - - - - - - - - -");
+            System.out.println("Building Index File for "+ myPath);
+            Document index = mkIndexDoc(startingPoint,_base,parentPath);
 
             XMLOutputter xmlo = new XMLOutputter(Format.getPrettyFormat());
 
             String s3IndexId = pathConcat(myPath,"/index.xml");
-            System.out.println("Index File s3Id: "+s3IndexId);
-            System.out.println(xmlo.outputString(index));
+            System.out.println("Index File s3Id: "+s3IndexId+"\n");
+            System.out.print(xmlo.outputString(index));
+
             File tmpIndexFile = new File("/tmp/s3IndexTmpFile.xml");
             if(tmpIndexFile.exists() && tmpIndexFile.canWrite()){
                 if(!tmpIndexFile.delete())
@@ -223,33 +271,33 @@ public class SimpleRepositoryUploader {
                 FileOutputStream fos  = new FileOutputStream(tmpIndexFile);
                 xmlo.output(index,fos);
                 fos.close();
-
                 System.out.println("[S3 INDEX UPLOAD POINT] fileName: " + tmpIndexFile.getCanonicalPath() +
-                        "  s3IndexId: " + s3IndexId +
-                        "");
+                        "  s3IndexId: " + s3IndexId );
                 if(!_dryRun) {
-                    _simpleS3Uploader.uploadFile(tmpIndexFile,s3IndexId);
+                    _simpleS3Uploader.uploadFile(tmpIndexFile,s3IndexId,makePubliclyReadable);
                 }
                 else {
                     System.out.println("** [DRY-RUN] NO S3 ACTION TAKEN");
 
                 }
+                System.out.println("Index creation and transmission completed.");
+
             }
-
-
-            for(File child : f.listFiles()){
+            for(File child : startingPoint.listFiles()){
                 if(!child.isDirectory()){
-                    if(f.canRead()){
+                    if(startingPoint.canRead()){
                         String s3Id = pathConcat(myPath,child.getName());
+                        System.out.println("- - - - - - - - - - - - - - - - -");
                         System.out.println("[S3 FILE UPLOAD POINT] fileName: "+child.getCanonicalPath()+"  s3Id: "+s3Id);
                         if(!_dryRun) {
                             System.out.println("Uploading file: "+child.getCanonicalPath()+ "  to S3 bucket "+_s3BucketName);
-                            _simpleS3Uploader.uploadFile(child,s3Id);
+                            _simpleS3Uploader.uploadFile(child,s3Id,makePubliclyReadable);
                         }
                         else {
                             System.out.println("** [DRY-RUN] NO S3 ACTION TAKEN");
 
                         }
+                        System.out.println("File transmission completed.");
                     }
                     else {
                         throw new BadIngestException("Unable to read file: "+child.getName());
@@ -257,11 +305,10 @@ public class SimpleRepositoryUploader {
                 }
                 else {
                     // Recursive index generation
-                    s3Ingest(child,myPath, level+1);
+                    s3Ingest(child,myPath, makePubliclyReadable, level+1);
                 }
             }
         }
-        System.out.println("");
 
     }
 
