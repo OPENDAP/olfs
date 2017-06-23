10db9f5625eb2f0ba166b3ffe1818c14ca4ef66d
Nathan Potter <ndp@opendap.org>
2017-06-23 14:52:01 -0700

    More templating for large merra2 colection

--- resources/WCS/2.0/templates/buildMerra2.sh
+++ resources/WCS/2.0/templates/buildMerra2.sh
@@ -5,25 +5,31 @@
 # ./buildMerra2.sh MERRA2_400.tavgM_2d_int_Nx
 
 collection=$1
-echo $collection
+echo "COLLECTION: $collection";
 
 files=`curl -s https://s3.amazonaws.com/testbed-13/merra2//index.xml | grep $collection | awk '{split($2,f,"\"");print f[2];}'`;
-echo $files;
+echo FILES: $files;
 
-cat LFC_OPEN.xml > tmp;
+cat LFC_OPEN.xml > tmp/LFC.xml;
 
 for file in $files ; do
-    echo Processing $file
+    echo "------------------------------------------"
+    echo Processing: $file
     #curl https://s3.amazonaws.com/testbed-13/merra2/$i > $i;
-
-
     # MERRA2_400.tavgM_2d_int_Nx.201310.nc4
 
     year=`echo $file | awk '{print substr($0,28,4)};'`;
     month=`echo $file | awk '{print substr($0,32,2)};'`;
-    date="$year-$month-01";
-    echo "DATE: $date"
-    cat LFC_MERRA2_400.tavgM_2d_int_Nx.xml | sed -e "s/@COVERAGE_NAME@/$file/g" -e "s/@DATE@/$date/g" >> tmp
+
+    start_date="$year-$month-01";
+    echo "START_DATE: $start_date"
+
+    end_date=`echo "$year $month" | awk '{y=$1+0.0;m=$2+0.0; if(m==12){y++;m=1;}else{m++;} printf("%04d-%02d-01",y,m)}'`
+    echo "END_DATE: $end_date"
+
+    cat LFC_MERRA2_400.tavgM_2d_int_Nx.xml | sed -e "s/@COVERAGE_NAME@/$file/g" -e "s/@DATE@/$start_date/g" >> tmp/LFC.xml
+
+    cat $collection.xml  |  sed -e "s/@MERRA2DATASET@/$file/g" -e "s/@START_DATE@/$start_date/g"  -e "s/@END_DATE@/$end_date/g" > tmp/$file.xml
 
 done
-cat LFC_CLOSE.xml >> tmp;
+cat LFC_CLOSE.xml >> tmp/LFC.xml;
