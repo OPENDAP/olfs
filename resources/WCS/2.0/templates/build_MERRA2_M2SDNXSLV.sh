e215533696ee3685033446458049b50e95da2cf0
ndp-opendap <ndp@opendap.org>
2017-08-21 12:21:00 -0700

    Sync

--- resources/WCS/2.0/templates/build_MERRA2_M2SDNXSLV.sh
+++ resources/WCS/2.0/templates/build_MERRA2_M2SDNXSLV.sh
@@ -16,7 +16,7 @@ cat LFC_OPEN.xml > tmp/LFC_$template;
 
 # The data start at 1980, but I'm just getting from 2014 on because the 
 # daily precipitation data on extends that far into the past
-for year in {2014..2017}; do
+for year in {1980..1981}; do
     echo "Processing year $year"
     for month in 01 02 03 04 05 06 07 08 09 10 11 12 ; do
         echo "Processing Month: $month"
