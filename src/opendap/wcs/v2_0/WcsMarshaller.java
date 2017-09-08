fa05a92c29f662ee7c6c69162afbb178be956ab3
ndp-opendap <ndp@opendap.org>
2017-09-08 10:21:31 -0700

    Added calls to set Field Id but to no avial, commented them out and left them in place.

--- src/opendap/wcs/v2_0/WcsMarshaller.java
+++ src/opendap/wcs/v2_0/WcsMarshaller.java
@@ -668,6 +668,7 @@ public class WcsMarshaller {
 
             dataRecord1FieldQuantity.setDefinition("urn:ogc:def:dataType:OGC:1.1:measure");
             dataRecord1FieldQuantity.setDescription("large_scale_rainfall");
+            //dataRecord1FieldQuantity.setId("large_scale_rainfall");
 
             net.opengis.swecommon.v_2_0.UnitReference dataRecord1FieldQuantityUom = new net.opengis.swecommon.v_2_0.UnitReference();
             dataRecord1FieldQuantityUom.setCode("kg m-2 s-1");
@@ -851,6 +852,7 @@ public class WcsMarshaller {
 
         dataRecord1FieldQuantity.setDefinition("urn:ogc:def:dataType:OGC:1.1:measure");
         dataRecord1FieldQuantity.setDescription(attributesHash.get("long_name").getValue());
+        // dataRecord1FieldQuantity.setId(var.getName());
 
         net.opengis.swecommon.v_2_0.UnitReference dataRecord1FieldQuantityUom = new net.opengis.swecommon.v_2_0.UnitReference();
         dataRecord1FieldQuantityUom.setCode(attributesHash.get("units").getValue());
