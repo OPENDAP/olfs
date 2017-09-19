43f3ad0547560b9414773b37a846788b0fb192d1
ndp-opendap <ndp@opendap.org>
2017-09-19 08:43:45 -0700

    sync

--- resources/WCS/2.0/downloads/get_collections.sh
+++ resources/WCS/2.0/downloads/get_collections.sh
@@ -4,7 +4,7 @@ years="1980 1985 1990 1995 2000 2005 2010 2015"
 months="07"
 
 auth=" --load-cookies tmp/urs_cookies --save-cookies tmp/urs_cookies --keep-session-cookies ";
-wget_opts=" –r -c -nH -nd -np -A nc4";
+wget_opts=" -r -c -nH -nd -np -A nc4";
 
 # wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --keep-session-cookies -r -c -nH -nd -np -A nc4,xml "https://goldsmr4.gesdisc.eosdis.nasa.gov/data/MERRA2_MONTHLY/M2TMNXSLV.5.12.4/1981/"
 
