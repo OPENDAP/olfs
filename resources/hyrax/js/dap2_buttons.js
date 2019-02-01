b93271f4ce340415f89f7eca4755748ef7651205
Nathan Potter <ndp@opendap.org>
2019-02-01 10:15:42 -0800

    Added dialog box to both IFH stacks that asks users to select variables before accessing data.

--- resources/hyrax/js/dap2_buttons.js
+++ resources/hyrax/js/dap2_buttons.js
@@ -13,7 +13,7 @@ function ascii_button() {
     } else {
         make_a_selection();
         return;
-        // var ascii_url = url_parts[0] + ".ascii";
+        // var ascii_url = url_parts[0] + ".ascii?";
     }
 
     window.open(encodeURI(ascii_url), "ASCII_Data");
@@ -26,7 +26,9 @@ function covjson_button() {
     if (url_parts[1] != null) {
         var covjson_url = url_parts[0] + ".covjson?" + url_parts[1];
     } else {
-        var covjson_url = url_parts[0] + ".covjson?";
+        make_a_selection();
+        return;
+        // var covjson_url = url_parts[0] + ".covjson?";
     }
 
     window.open(encodeURI(covjson_url), "CoverageJSON Data");
@@ -44,7 +46,9 @@ function netcdf_button(ext) {
     if (url_parts[1] != null) {
         var binary_url = url_parts[0] + "." + ext + "?" + url_parts[1];
     } else {
-        var binary_url = url_parts[0] + "." + ext + "?";
+        make_a_selection();
+        return;
+        // var binary_url = url_parts[0] + "." + ext + "?";
     }
 
     window.location = encodeURI(binary_url);
@@ -62,7 +66,9 @@ function binary_button(ext) {
     if (url_parts[1] != null) {
         var binary_url = url_parts[0] + "." + ext + "?" + url_parts[1];
     } else {
-        var binary_url = url_parts[0] + "." + ext + "?";
+        make_a_selection();
+        return;
+        // var binary_url = url_parts[0] + "." + ext + "?";
     }
 
     window.location = encodeURI(binary_url);
