4e6f75abdb961cc8663a468f9d7427fe839d18ee
Nathan Potter <ndp@opendap.org>
2019-03-08 12:35:54 -0800

    sync

--- resources/experiments/NodeCache/node_cache_test.sh
+++ resources/experiments/NodeCache/node_cache_test.sh
@@ -4,16 +4,21 @@ target_hyrax="http://test.opendap.org:8080/"
 
 # Get site map:
 
-siteMapFileList=`curl -s "${target_hyrax}/robots.txt" | awk '{print $2}' - `
+# siteMapFileList=`curl -s "${target_hyrax}/robots.txt" | awk '{print $2}' - `
 
 # echo "${siteMapList}"
 
-for siteMapFileUrl in ${siteMapFileList}
-do
+#for siteMapFileUrl in ${siteMapFileList}
+#do
     echo "Site Map File: ${siteMapFileUrl}"
 
-    siteMapContent="`curl -s ${siteMapFileUrl}`"
+    # siteMapContent="`curl -s ${siteMapFileUrl} | tee items`"
 
-    echo "SITE MAP CONTENT:"
-    echo "${siteMapContent}"
-done
+
+    let count=0
+    for item in `cat items`
+    do
+        echo "${count}: ${item}"
+        ((count+=1))
+    done
+#done
