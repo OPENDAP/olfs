16c645788b0df36673fa2b7bf0083d375d463c86
Slav Korolev <slav.korolev@nasa.gov>
2020-11-20 10:27:42 -0500

    HYRAX-314. Added run-snyk.sh script.

---
+++ run-snyk.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+snyk test --severity-threshold=high
+snykjson=$(snyk test --severity-threshold=high --json)
+isUpgradable=$(jq '.vulnerabilities[].isUpgradable | tostring | select(. == "true")' <<< $snykjson)
+
+if [[ $isUpgradable != '' ]]; then
+    echo "To fix a problem you should change version of library in the build.gradle file
+    and download library of new version into lib folder."
+else
+    exit 0
+fi
