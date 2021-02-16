3ec745cf52d0f157f9ebbb97d7447a46ffa0ab8e
ndp-opendap <ndp@opendap.org>
2021-02-16 08:11:03 -0800

    Added robots.base to olfs config

--- src/opendap/bes/BesSiteMap.java
+++ src/opendap/bes/BesSiteMap.java
@@ -9,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Date;
 import java.util.TreeSet;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -23,8 +25,10 @@ public class BesSiteMap {
     private static final Logger LOG = LoggerFactory.getLogger(BesSiteMap.class);
 
     public static final String SITE_MAP_CACHE_ELEMENT_NAME = "SiteMapCache";
+
     public static final String REFRESH_INTERVAL_ATTRIBUTE_NAME = "refreshInterval";
     public static final String CACHE_FILE_ATTRIBUTE_NAME = "cacheFile";
+    public static final String ROBOTS_BASE_ATTRIBUTE_NAME = "robotsBaseFile";
 
     private static final ReentrantReadWriteLock cacheLock = new ReentrantReadWriteLock();
     private static final AtomicBoolean ENABLED = new AtomicBoolean(false);
@@ -39,6 +43,10 @@ public class BesSiteMap {
 
     public static final String  DEFAULT_CACHE_FILE = "/etc/olfs/cache/SiteMap.cache"; // this is a punt
     private static String SiteMapCacheFileName = DEFAULT_CACHE_FILE;
+
+    public static final String  DEFAULT_ROBOTS_BASE_FILE = "/etc/olfs/robots.base"; // this is a punt
+    private static String RobotsBaseText = "";
+
 
     public static final long DEFAULT_CACHE_REFRESH_INTERVAL = 600; // Ten minutes worth of seconds.
     private static AtomicLong cacheRefreshInterval_ms = new AtomicLong(DEFAULT_CACHE_REFRESH_INTERVAL * 1000);
@@ -68,7 +76,7 @@ public class BesSiteMap {
         siteMap = new TreeSet<>();
         siteMapFileCount = 1;
         this.dapServicePrefix = dapServicePrefix;
-        siteMapCharCount = getSiteMap();
+        siteMapCharCount = getSiteMapItemCount();
         creation = new Date();
     }
 
@@ -116,7 +124,13 @@ public class BesSiteMap {
             cacheFileName = cacheFileNameString;
         }
 
-        init(cacheFileName, refreshIntervalSeconds);
+        String rbFileName = DEFAULT_ROBOTS_BASE_FILE;
+        String robotsBaseAttrValue = config.getAttributeValue(ROBOTS_BASE_ATTRIBUTE_NAME);
+        if(robotsBaseAttrValue!=null){
+            rbFileName = robotsBaseAttrValue;
+        }
+
+        init(rbFileName, cacheFileName, refreshIntervalSeconds);
     }
 
 
@@ -127,7 +141,7 @@ public class BesSiteMap {
      * @param refreshIntervalSeconds The time that a site map is considered
      *                               valid, after which it must be refreshed.
      */
-    public static void init(String cacheFileName, long refreshIntervalSeconds)
+    public static void init(String robotsBaseFilename, String cacheFileName, long refreshIntervalSeconds)
             throws BadConfigurationException {
         cacheLock.writeLock().lock();
         try {
@@ -143,9 +157,16 @@ public class BesSiteMap {
             cacheRefreshInterval_ms.set(refreshIntervalSeconds * 1000);
 
             SiteMapCacheFileName = cacheFileName;
-
             checkSiteMapFileLocation();
 
+            try {
+                byte[] encoded = Files.readAllBytes(Paths.get(robotsBaseFilename));
+                RobotsBaseText = new String(encoded, HyraxStringEncoding.getCharset());
+            }
+            catch (IOException e) {
+                LOG.error("Failed to read robots base file: {} , Message: {} SKIPPING.",robotsBaseFilename,e.getMessage());
+            }
+            RobotsBaseText += "\n";
             ENABLED.set(true);
             LOG.debug("INITIALIZED  SiteMapCacheFile: {}  RefreshInterval: {} s",
                     SiteMapCacheFileName,
@@ -197,7 +218,7 @@ public class BesSiteMap {
      * @throws PPTException
      * @throws IOException
      */
-    private long getSiteMap() throws BESError, BadConfigurationException, PPTException, IOException {
+    private long getSiteMapItemCount() throws BESError, BadConfigurationException, PPTException, IOException {
 
         File smcFile = new File(SiteMapCacheFileName);
 
@@ -374,7 +395,7 @@ public class BesSiteMap {
      */
     public String getSiteMapEntryForRobotsDotText(String siteMapServicePrefix ) throws IOException {
 
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(RobotsBaseText);
         log.debug("Building siteMap files index response.");
         for(long i = 0; i < siteMapFileCount; i++){
             sb.append("sitemap: ").append(siteMapServicePrefix).append("/").append(PseudoFileOpener).append(Long.toString(i)).append(PseudoFileCloser).append("\n");
