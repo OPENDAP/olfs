813fd972c88ec5781e60a0b3b153c4b5553b7bad
ndp-opendap <ndp@opendap.org>
2021-08-03 10:48:54 -0700

    Retired the remaining code from the noaa s3 experiment

---
+++ retired/src/opendap/aws/glacier/Download.java
@@ -0,0 +1,270 @@
+/*
+ * /////////////////////////////////////////////////////////////////////////////
+ * // This file is part of the "Hyrax Data Server" project.
+ * //
+ * //
+ * // Copyright (c) 2013 OPeNDAP, Inc.
+ * // Author: Nathan David Potter  <ndp@opendap.org>
+ * //
+ * // This library is free software; you can redistribute it and/or
+ * // modify it under the terms of the GNU Lesser General Public
+ * // License as published by the Free Software Foundation; either
+ * // version 2.1 of the License, or (at your option) any later version.
+ * //
+ * // This library is distributed in the hope that it will be useful,
+ * // but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * // Lesser General Public License for more details.
+ * //
+ * // You should have received a copy of the GNU Lesser General Public
+ * // License along with this library; if not, write to the Free Software
+ * // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+ * //
+ * // You can contact OPeNDAP, Inc. at PO Box 112, Saunderstown, RI. 02874-0112.
+ * /////////////////////////////////////////////////////////////////////////////
+ */
+
+package opendap.aws.glacier;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.services.glacier.AmazonGlacierAsyncClient;
+import com.amazonaws.services.glacier.model.*;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.util.Date;
+
+
+
+
+public abstract class Download implements Serializable {
+
+    Logger _log;
+
+    private boolean _started;
+
+
+    Date _startDate;
+    private long _expectedDelay;   // In seconds
+    InitiateJobResult _initiateJobResult;
+
+    private String _vaultName;
+    private String _endPointUrl;
+
+    private AWSCredentials _credentials;
+
+    private File _downloadFile;
+
+    private String _identifier;
+
+
+    public Download(String vaultName, String glacierEndpointUrl, AWSCredentials awsCredentials, long expectedDelay) {
+
+        _log = LoggerFactory.getLogger(this.getClass());
+
+        _vaultName = vaultName;
+        _endPointUrl = glacierEndpointUrl;
+        _credentials = awsCredentials;
+        _expectedDelay = expectedDelay;
+
+        _startDate = null;
+        _initiateJobResult = null;
+        _started = false;
+        _identifier = null;
+    }
+
+    public boolean started() {
+        return _started;
+    }
+
+
+    public Date getStartDate(){ return new Date(_startDate.getTime()); }
+
+    public long getExpectedDelay(){ return _expectedDelay; }
+
+    public String getVaultName() { return _vaultName;}
+    public String getEndpointUrl() { return _endPointUrl; }
+    public AWSCredentials getCredentials() { return _credentials; }
+
+    public InitiateJobResult getInitiateJobResult(){ return _initiateJobResult; }
+
+
+    public void setDownloadFile(File file){
+        _downloadFile = new File(file.getAbsolutePath());
+    }
+
+    public File getDownloadFile() throws IOException {
+        if(_downloadFile==null)  {
+            String msg = "Download file has not been set for this Download!";
+            _log.error(msg);
+            throw new IOException(msg);
+        }
+        return new File(_downloadFile.getAbsolutePath());
+    }
+
+
+
+
+
+    public String getDownloadId() { return _identifier; }
+
+    public void setDownloadId(String id) {
+        if(_identifier!=null){
+            _log.warn("setDownloadId() - Download identifier cannot be set to '{}' because it has already been set to '{}'",id,_identifier);
+        } else
+            _identifier = id;
+    }
+
+
+
+        // Returns Estimated wait time in seconds
+    public long estimatedTimeRemaining() throws IOException {
+
+        if(_startDate == null){
+            throw new IOException("Glacier Job has not been started!");
+        }
+
+        long start = getStartDate().getTime();
+        long now = new Date().getTime();
+
+        long elapsed = now - start;
+
+        long remaining  = (getExpectedDelay()*1000) - elapsed;
+        if(remaining<0)
+            remaining = 0;
+
+        return remaining/1000;
+
+    }
+
+
+    public abstract boolean startJob() throws IOException;
+
+    protected boolean startJob(JobParameters jobParameters) throws IOException {
+
+        _log.debug("startJob() - BEGIN ");
+
+        if(_started)
+            throw new IOException("Glacier inventory retrieval job has already been started!");
+
+        AmazonGlacierAsyncClient client = new AmazonGlacierAsyncClient(getCredentials());
+        client.setEndpoint(getEndpointUrl());
+
+        InitiateJobRequest ijb = new InitiateJobRequest(getVaultName(),jobParameters);
+        try {
+
+            _startDate = new Date();
+
+            _initiateJobResult = client.initiateJob(ijb);
+
+
+            _log.debug("startJob() - Glacier downloadJobOutput job started. jobId: {}",_initiateJobResult.getJobId());
+
+            _started = true;
+
+        }
+        catch (Exception e) {
+            _log.error("startJob() - Download failed to start! msg: {}",e.getMessage());
+        }
+
+        _log.debug("startJob() - END ");
+
+        return _started;
+
+    }
+
+
+    public boolean jobCompleted() throws IOException {
+
+        _log.debug("archiveRetrievalJobCompleted() - BEGIN ");
+
+        if(!_started)
+            throw new IOException("Glacier retrieval job has NOT been started!");
+
+
+        AmazonGlacierAsyncClient client = new AmazonGlacierAsyncClient(getCredentials());
+        client.setEndpoint(getEndpointUrl());
+
+
+        InitiateJobResult initiateJobResult = getInitiateJobResult();
+
+        long timeRemaining = estimatedTimeRemaining();
+
+        _log.debug("archiveRetrievalJobCompleted() - Estimated Time Remaining: {} seconds  jobId: {}",timeRemaining,initiateJobResult.getJobId());
+
+        DescribeJobRequest djr = new DescribeJobRequest(getVaultName(),initiateJobResult.getJobId());
+
+        DescribeJobResult describeJobResult = client.describeJob(djr);
+
+        _log.debug("archiveRetrievalJobCompleted() - DescribeJobResult: {}",describeJobResult.toString());
+        _log.debug("archiveRetrievalJobCompleted() - DescribeJobResult.isCompleted(): {}",describeJobResult.isCompleted());
+        _log.debug("archiveRetrievalJobCompleted() - DescribeJobResult.status(): {}",describeJobResult.getStatusCode());
+
+        _log.debug("archiveRetrievalJobCompleted() - END ");
+
+        return describeJobResult.isCompleted();
+
+    }
+
+
+    public boolean downloadJobOutput() throws IOException {
+
+        return downloadJobOutput(getDownloadFile());
+    }
+
+
+
+
+    public boolean downloadJobOutput(File downloadFile) throws IOException {
+
+
+        if(!jobCompleted()) {
+            _log.warn("Glacier retrieval job has not completed!");
+            return false;
+        }
+        boolean success = false;
+
+
+        String jobId = getInitiateJobResult().getJobId();
+
+        _log.debug("downloadJobOutput() - BEGIN (Retrieving Glacier Job Result. JobID: {})",jobId);
+
+
+        try {
+            AmazonGlacierAsyncClient client = new AmazonGlacierAsyncClient(getCredentials());
+            client.setEndpoint(getEndpointUrl());
+
+
+            GetJobOutputRequest jobOutputRequest = new GetJobOutputRequest()
+                    .withJobId(jobId)
+                    .withVaultName(getVaultName());
+            GetJobOutputResult jobOutputResult = client.getJobOutput(jobOutputRequest);
+
+            InputStream in = jobOutputResult.getBody();
+
+            FileOutputStream out = new FileOutputStream(downloadFile);
+            IOUtils.copy(in, out);
+
+            _log.error("downloadJobOutput() - Retrieved Glacier job output. CacheFile: {}",downloadFile.getAbsolutePath());
+
+            success = true;
+
+        }
+        catch (Exception e) {
+            _log.error("downloadJobOutput() - Failed to retrieve Glacier job output. Msg: {}",e.getMessage());
+        }
+
+
+
+        _log.debug("downloadJobOutput() - END");
+
+
+        return success;
+
+    }
+
+
+
+}
---
+++ retired/src/opendap/aws/s3/SimpleRepositoryUploader.java
@@ -0,0 +1,326 @@
+package opendap.aws.s3;
+
+import opendap.noaa_s3.S3Index;
+import org.apache.commons.cli.*;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.ProcessingInstruction;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+/**
+ * Created by ndp on 8/31/16.
+ */
+public class SimpleRepositoryUploader {
+
+    private SimpleS3Uploader _simpleS3Uploader;
+
+    private static final String s3Url = "https://s3.amazonaws.com/";
+    private  String _delimiter = "/";
+    private  String _base = null;
+
+    private boolean _verbose;
+    private boolean _dryRun;
+
+    private String _s3BucketName;
+    private String _awsAccessKeyId;
+    private String _awsSecretKey;
+    private String _repoDirectoy;
+    private boolean _makePubliclyReadable;
+
+
+
+    private  boolean processCommandline(String[] args) throws Exception {
+
+        CommandLineParser parser = new PosixParser();
+
+        Options options = new Options();
+
+        options.addOption("h", "help", false, "Usage information.");
+        options.addOption("p", "public", false, "Makes all of the files uploaded publicly readable");
+        options.addOption("v", "verbose", false, "Makes more output...");
+        options.addOption("t", "dry-run", false, "Does everything but actually push stuff into S3 ");
+
+        options.addOption("i", "awsId", true, "AWS access key ID for working with S3.");
+        options.addOption("s", "awsKey", true, "AWS secret key for working with S3.");
+        options.addOption("b", "s3-bucket-name", true, "Name of S3 bucket on which to operate.");
+        options.addOption("d", "directory", true, "The fully qualified path to the top directory of " +
+                "the tree you want to ingest into S3..");
+
+
+        CommandLine line =   parser.parse(options, args);
+
+        String usage  = this.getClass().getSimpleName()+" [-h] [-p] [-v] [-t] -i AWSAccessKeyID -s AWSSecretKey -b S3BucketName -d repoDirectoryRoot ";
+
+        StringBuilder errorMessage = new StringBuilder();
+
+        if (line.hasOption("help")) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp(usage, options);
+            return false;
+        }
+
+        _verbose = line.hasOption("verbose");
+        System.out.println("Verbose mode: "+(_verbose?"ENABLED":"DISABLED"));
+
+        _dryRun = line.hasOption("dry-run");
+        System.out.println("Dry-Run mode: "+(_dryRun?"ENABLED":"DISABLED"));
+
+        _makePubliclyReadable = line.hasOption("public");
+        System.out.println("Access: "+(_makePubliclyReadable?"Public":"Private"));
+
+        /*
+        s3LocalCacheRoot = line.getOptionValue("s3-root");
+        if(s3LocalCacheRoot==null){
+            errorMessage.append("Missing Parameter - You must provide an S3 cache directory with the --s3-root option.\n");
+        }
+        */
+
+        _s3BucketName = line.getOptionValue("s3-bucket-name");
+        if(_s3BucketName ==null){
+            errorMessage.append("Missing Parameter - You must provide a S3 bucket name with the --s3-bucket-name option.\n");
+        }
+        _base = s3Url + _s3BucketName;
+
+
+        _awsAccessKeyId =  line.getOptionValue("awsId");
+        if(_awsAccessKeyId == null){
+            errorMessage.append("Missing Parameter - You must provide an AWS access key ID (to access the S3 service) with the --awsId option.\n");
+        }
+
+        _awsSecretKey = line.getOptionValue("awsKey");
+        if(_awsSecretKey == null){
+            errorMessage.append("Missing Parameter - You must provide an AWS secret key (to access the S3 service) with the --awsKey option.\n");
+        }
+
+        _repoDirectoy = line.getOptionValue("directory");
+        if (_repoDirectoy ==null) {
+            errorMessage.append("Missing Parameter - You must provide an source directory (with the -d option) from which to build the S3 repo.\n");
+        }
+
+        if(errorMessage.length()!=0){
+            System.err.println(errorMessage);
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp(usage, options);
+            return false;
+        }
+
+
+       _simpleS3Uploader = new SimpleS3Uploader(_s3BucketName,_awsAccessKeyId,_awsSecretKey);
+
+
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            SimpleRepositoryUploader srb = new SimpleRepositoryUploader();
+            if(srb.processCommandline(args)){
+                File f = new File(srb._repoDirectoy);
+                srb.s3Ingest(f);
+            }
+        }
+        catch (Throwable t){
+            System.err.println("Caught "+t.getClass().getName()+"  Message: "+t.getMessage());
+            t.printStackTrace(System.err);
+        }
+    }
+
+
+
+
+
+
+
+    /**
+     * Builds an index for the passed directory. If the passed File is not a directory or cannot be
+     * This is an example of the index file that will be built.
+     *
+    <?xml version="1.0" encoding="UTF-8"?>
+    <?xml-stylesheet type='text/xsl' href='/opendap.test//index.xsl'?>
+    <index xmlns="http://nodc.noaa.gov/s3/catalog/1.0" base="https://s3.amazonaws.com/opendap.test" path="" name="opendap.test" delimiter="/" encoding="UTF-8">
+      <folder name="data" size="231402720" count="1"/>
+    </index>
+     *
+     *
+     * @param node The directory to index.
+     * @param base The value of the base attribute for this index.
+     * @param parentPath  The index/catalog path to the parent directory.
+     * @return The index document object.
+     * @throws BadIngestException When the passed file is not a directory or is not readable.
+     */
+    public Document mkIndexDoc(File node, String base, String parentPath) throws BadIngestException {
+
+
+        if(!node.isDirectory())
+            throw new BadIngestException("mkIndexDoc() - The file "+node.getName()+" is not a directory");
+
+        if(!node.canRead())
+            throw new BadIngestException("mkIndexDoc() - The directory "+node.getName()+" cannot be read");
+
+        String path = _delimiter + pathConcat(parentPath , node.getName());
+
+
+        Element index = new Element("index", S3Index.S3_CATALOG_NAMESPACE);
+        index.setAttribute("base",base);
+        index.setAttribute("name",_s3BucketName);
+        index.setAttribute("path",path);
+        index.setAttribute("delimiter",_delimiter);
+        index.setAttribute("encoding","UTF-8");
+
+
+        for(File f: node.listFiles()){
+            Element indexElement;
+            if(f.isDirectory()){
+                indexElement = new Element("folder",S3Index.S3_CATALOG_NAMESPACE);
+            }
+            else {
+                indexElement = new Element("file",S3Index.S3_CATALOG_NAMESPACE);
+            }
+
+            indexElement.setAttribute("name",f.getName());
+            Date lmt = new Date(f.lastModified());
+            SimpleDateFormat outputFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+            indexElement.setAttribute("last-modified",outputFormatter.format(lmt));
+            indexElement.setAttribute("size",Long.toString(f.length()));
+            index.addContent(indexElement);
+
+        }
+
+        Document indexDoc = new Document();
+
+        HashMap<String,String> piMap = new HashMap<String,String>( 2 );
+        piMap.put( "type", "text/xsl" );
+        piMap.put( "href", "/opendap.test//index.xsl" );
+        ProcessingInstruction pi = new ProcessingInstruction( "xml-stylesheet", piMap );
+
+        indexDoc.addContent(pi);
+
+        indexDoc.setRootElement(index);
+
+        return indexDoc;
+    }
+
+
+    /**
+     * Beginning with {@code startingPoint} this recursive method crawls the filesystem building index files to
+     * represent each directory. These index files along with all of the regular files encountered in the crawl
+     * are uploaded to the S3 bucket identified through the {@code SimpleRepositoryUploader} object's state.
+     * The resulting index.xml files in S3 will provide the same logical view of the regular files as was
+     * manifested by the file system from which they were harvested.
+     *
+     * @param startingPoint The staring pint directry/file for the crawl.
+     * @throws BadIngestException
+     * @throws IOException
+     */
+    public void s3Ingest(File startingPoint) throws IOException, BadIngestException {
+        s3Ingest(startingPoint, "",_makePubliclyReadable,0);
+    }
+
+
+
+
+    /**
+     * Beginning with {@code startingPoint} this recursive method crawls the filesystem building index files to
+     * represent each directory. These index files along with all of the regular files encountered in the crawl
+     * are uploaded to the S3 bucket identified through the {@code SimpleRepositoryUploader} object's state.
+     * The resulting index.xml files in S3 will provide the same logical view of the regular files as was
+     * manifested by the file system from which they were harvested.
+     *
+     * @param startingPoint The staring pint directry/file for the crawl.
+     * @param parentPath The S3 navigation path tot he parent directory (typically set to "" at the begining of crawl)
+     * @param makePubliclyReadable If true the objects uploaded to S3 will be publicly readable. The default
+     *                             is false/private.
+     * @param level This is a recurisive level counter -
+     * @throws BadIngestException
+     * @throws IOException
+     */
+    public void s3Ingest(File startingPoint, String parentPath, boolean makePubliclyReadable, int level) throws BadIngestException, IOException {
+
+        String myPath = pathConcat(parentPath , startingPoint.getName());
+
+        System.out.println("");
+        System.out.println(level + " ----------------------------------------------------------- "+level);
+        System.out.println("ITEM: " + myPath + " (" + (startingPoint.isDirectory()?"dir":"file") + ")");
+        if (startingPoint.isDirectory()) {
+            System.out.println("- - - - - - - - - - - - - - - - -");
+            System.out.println("Building Index File for "+ myPath);
+            Document index = mkIndexDoc(startingPoint,_base,parentPath);
+
+            XMLOutputter xmlo = new XMLOutputter(Format.getPrettyFormat());
+
+            String s3IndexId = pathConcat(myPath,"/index.xml");
+            System.out.println("Index File s3Id: "+s3IndexId+"\n");
+            System.out.print(xmlo.outputString(index));
+
+            File tmpIndexFile = new File("/tmp/s3IndexTmpFile.xml");
+            if(tmpIndexFile.exists() && tmpIndexFile.canWrite()){
+                if(!tmpIndexFile.delete())
+                    throw new IOException("Unable to delete file: "+tmpIndexFile.getCanonicalPath());
+            }
+            if(tmpIndexFile.createNewFile()){
+                FileOutputStream fos  = new FileOutputStream(tmpIndexFile);
+                xmlo.output(index,fos);
+                fos.close();
+                System.out.println("[S3 INDEX UPLOAD POINT] fileName: " + tmpIndexFile.getCanonicalPath() +
+                        "  s3IndexId: " + s3IndexId );
+                if(!_dryRun) {
+                    _simpleS3Uploader.uploadFile(tmpIndexFile,s3IndexId,makePubliclyReadable);
+                }
+                else {
+                    System.out.println("** [DRY-RUN] NO S3 ACTION TAKEN");
+
+                }
+                System.out.println("Index creation and transmission completed.");
+
+            }
+            for(File child : startingPoint.listFiles()){
+                if(!child.isDirectory()){
+                    if(startingPoint.canRead()){
+                        String s3Id = pathConcat(myPath,child.getName());
+                        System.out.println("- - - - - - - - - - - - - - - - -");
+                        System.out.println("[S3 FILE UPLOAD POINT] fileName: "+child.getCanonicalPath()+"  s3Id: "+s3Id);
+                        if(!_dryRun) {
+                            System.out.println("Uploading file: "+child.getCanonicalPath()+ "  to S3 bucket "+_s3BucketName);
+                            _simpleS3Uploader.uploadFile(child,s3Id,makePubliclyReadable);
+                        }
+                        else {
+                            System.out.println("** [DRY-RUN] NO S3 ACTION TAKEN");
+
+                        }
+                        System.out.println("File transmission completed.");
+                    }
+                    else {
+                        throw new BadIngestException("Unable to read file: "+child.getName());
+                    }
+                }
+                else {
+                    // Recursive index generation
+                    s3Ingest(child,myPath, makePubliclyReadable, level+1);
+                }
+            }
+        }
+
+    }
+
+    private  String pathConcat(String parentPath, String child){
+
+        while(parentPath.endsWith(_delimiter)){
+            parentPath = parentPath.substring(0,parentPath.length()-1);
+        }
+        if(parentPath.isEmpty())
+            return child;
+        else
+            return parentPath + _delimiter + child;
+    }
+
+}
