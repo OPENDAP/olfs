af2e214481c3ab07c8729a481fa089a1b21a5f15
ndp-opendap <ndp@opendap.org>
2022-07-22 09:21:42 -0700

    olfs: Updating olfs version and release files to olfs version 1.18.12 for hyrax-1.16.8

--- travis/versions_and_build_offsets.sh
+++ travis/versions_and_build_offsets.sh
@@ -4,7 +4,7 @@
 # to the current Travis number so that the 'build number' in
 # x.y.z-<build number> is zero. jhrg 3/22/21
 
-OLFS_VERSION=1.18.13
+OLFS_VERSION=1.18.12
 TRAVIS_OLFS_BUILD_OFFSET=1473
 echo "OLFS_VERSION: ${OLFS_VERSION}"
 echo "TRAVIS_OLFS_BUILD_OFFSET: ${TRAVIS_OLFS_BUILD_OFFSET}"
