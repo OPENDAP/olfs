540280a027bb0da94734718515c8d7758d1483f1
ndp-opendap <ndp@opendap.org>
2022-09-07 15:22:33 -0700

    Adding tagging step to hyrax-docker trogger activity

--- travis/trigger-hyrax-docker.sh
+++ travis/trigger-hyrax-docker.sh
@@ -6,23 +6,29 @@
 # separately).
 
 set -e
-
 echo "-- -- -- -- -- -- -- -- -- after_deploy BEGIN -- -- -- -- -- -- -- -- --"
 
-# This will get both the BES and libdap version numbers
-BES_SNAPSHOT=`cat bes-snapshot`;
-
-echo "New OLFS Web Archive snapshot has been pushed. Triggering the Docker build"
-
-git clone https://github.com/opendap/hyrax-docker
 git config --global user.name "The-Robot-Travis"
 git config --global user.email "npotter@opendap.org"
+
+# This will get both the BES and libdap version numbers
+BES_SNAPSHOT=$(cat ./bes-snapshot)
+
+OLFS_SNAPSHOT_TAG="olfs-${OLFS_BUILD_VERSION} "`date "+%FT%T%z"`
+HYRAX_SNAPSHOT_TAG="hyrax-${HYRAX_BUILD_VERSION} "`date "+%FT%T%z"`
+
+echo "New OLFS Web Archive snapshot has been pushed."
+echo "Tagging olfs with version: ${OLFS_BUILD_VERSION}"
+git tag -m "olfs-${OLFS_BUILD_VERSION}" -a "${OLFS_BUILD_VERSION}"
+git push "https://${GIT_UID}:${GIT_PSWD}@github.com/OPENDAP/olfs.git" "${OLFS_BUILD_VERSION}"
+
+echo "New OLFS has been pushed and tagged. Triggering the Docker build"
+
+git clone https://github.com/opendap/hyrax-docker
 
 cd hyrax-docker
 git checkout master
 
-OLFS_SNAPSHOT_TAG="olfs-${OLFS_BUILD_VERSION} "`date "+%FT%T%z"`
-HYRAX_SNAPSHOT_TAG="hyrax-${HYRAX_BUILD_VERSION} "`date "+%FT%T%z"`
 
 echo "${BES_SNAPSHOT}" > snapshot.time
 echo "${OLFS_SNAPSHOT_TAG}" >> snapshot.time
