811e94f64e5d0fc7ceb3c9a675ad3bbc523ff481
ndp-opendap <ndp@opendap.org>
2022-09-07 17:31:26 -0700

    Added byrax version tag to the hyrax-docker repo when triggering build

--- travis/trigger-hyrax-docker.sh
+++ travis/trigger-hyrax-docker.sh
@@ -6,7 +6,9 @@
 # separately).
 
 set -e
-echo "-- -- -- -- -- -- -- -- -- after_deploy BEGIN -- -- -- -- -- -- -- -- --"
+echo "# -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --"
+echo "# trigger-hyrax-docker.sh"
+echo "#"
 
 git config --global user.name "The-Robot-Travis"
 git config --global user.email "npotter@opendap.org"
@@ -17,12 +19,12 @@ BES_SNAPSHOT=$(cat ./bes-snapshot)
 OLFS_SNAPSHOT_TAG="olfs-${OLFS_BUILD_VERSION} "`date "+%FT%T%z"`
 HYRAX_SNAPSHOT_TAG="hyrax-${HYRAX_BUILD_VERSION} "`date "+%FT%T%z"`
 
-echo "New OLFS Web Archive snapshot has been pushed."
-echo "Tagging olfs with version: ${OLFS_BUILD_VERSION}"
+echo "# New OLFS Web Archive snapshot has been pushed."
+echo "# Tagging olfs with version: ${OLFS_BUILD_VERSION}"
 git tag -m "olfs-${OLFS_BUILD_VERSION}" -a "${OLFS_BUILD_VERSION}"
 git push "https://${GIT_UID}:${GIT_PSWD}@github.com/OPENDAP/olfs.git" "${OLFS_BUILD_VERSION}"
 
-echo "New OLFS has been pushed and tagged. Triggering the Docker build"
+echo "# New OLFS has been pushed and tagged. Triggering the Docker build..."
 
 git clone https://github.com/opendap/hyrax-docker
 
@@ -36,6 +38,7 @@ echo "${HYRAX_SNAPSHOT_TAG}" >> snapshot.time
 
 cat snapshot.time
 
+
 # Bounding the commit message with the " character allows use to include
 # new line stuff for easy commit message readability later.
 git commit -am \
@@ -45,9 +48,13 @@ ${BES_SNAPSHOT}
 ${OLFS_SNAPSHOT_TAG}
 ${HYRAX_SNAPSHOT_TAG}
 ";
-
 git status;
-git push https://$GIT_UID:$GIT_PSWD@github.com/opendap/hyrax-docker --all;
 
-echo "-- -- -- -- -- -- -- -- -- after_deploy END -- -- -- -- -- -- -- -- --"
+export hyrax_tag="hyrax-${HYRAX_BUILD_VERSION}"
+git tag -m "${hyrax_tag}" -a "${hyrax_tag}"
+
+git push "https://${GIT_UID}:${GIT_PSWD}@github.com/OPENDAP/hyrax-docker.git" --all
+git push "https://${GIT_UID}:${GIT_PSWD}@github.com/OPENDAP/hyrax-docker.git" "${hyrax_tag}"
+
+echo "# -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --"
 
