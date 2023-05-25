718caa31de939e68dc6bdabe33a6ef2dca5d103b
ndp-opendap <ndp@opendap.org>
2023-05-25 14:37:06 -0700

    Dropping unneeded imports from TransmitCoordinator

--- src/opendap/coreServlet/TransmitCoordinator.java
+++ src/opendap/coreServlet/TransmitCoordinator.java
@@ -1,9 +1,5 @@
 package opendap.coreServlet;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
 public interface TransmitCoordinator {
     boolean isCommitted();
     void reset() throws IllegalStateException;
