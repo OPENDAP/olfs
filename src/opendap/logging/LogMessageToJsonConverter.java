26335838f9dfc71b080b70818d85a2b49f23b5e4
ndp-opendap <ndp@opendap.org>
2025-06-09 09:45:49 -0700

    Simplified the implementation by not wrestling with gson's ouput.

--- src/opendap/logging/LogMessageToJsonConverter.java
+++ src/opendap/logging/LogMessageToJsonConverter.java
@@ -4,16 +4,17 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
 import com.google.gson.Gson;
 
 public class LogMessageToJsonConverter extends ClassicConverter {
+
+    /**
+     *
+     * @param event The logging event in question.
+     * @return The event's message string encoded as a JSON string. NOTE: The returned string value is always enclosed
+     * in double quote '"' characters.
+     */
     @Override
     public String convert(ILoggingEvent event) {
         Gson gson = new Gson();
-        String jsonStr = gson.toJson(event.getFormattedMessage());
-        if(jsonStr.startsWith("\"") && jsonStr.length()>1)
-            jsonStr=jsonStr.substring(1);
-
-        if(jsonStr.endsWith("\"") && jsonStr.length()>1 && !jsonStr.endsWith("\\\""))
-            jsonStr=jsonStr.substring(0,jsonStr.length()-1);
-
+        String jsonStr = gson.toJson(event.getFormattedMessage(), String.class);
         return jsonStr;
      }
 }
