8401e462b87b6c38e58bc33159ce301921686689
ndp-opendap <ndp@opendap.org>
2025-12-30 11:29:34 -0800

    test-deploy tags

--- travis/compute_build_tags.sh
+++ travis/compute_build_tags.sh
@@ -13,23 +13,6 @@ echo "#" >&2
 source ./travis/versions_and_build_offsets.sh
 echo "#" >&2
 
-export OLFS_BUILD_NUMBER=$(expr $TRAVIS_BUILD_NUMBER - $TRAVIS_OLFS_BUILD_OFFSET)
-export OLFS_BUILD_VERSION="${OLFS_VERSION}-${OLFS_BUILD_NUMBER}"
-echo "#        OLFS_BUILD_VERSION: ${OLFS_BUILD_VERSION}" >&2
-
-export HYRAX_BUILD_NUMBER=$(expr $TRAVIS_BUILD_NUMBER - $TRAVIS_HYRAX_BUILD_OFFSET)
-export HYRAX_BUILD_VERSION="${HYRAX_VERSION}-${HYRAX_BUILD_NUMBER}"
-echo "#       HYRAX_BUILD_VERSION: ${HYRAX_BUILD_VERSION}" >&2
-echo "#" >&2
-
-test_deploy=""
-if [[ "$TRAVIS_BRANCH" == *"-test-deploy" ]]
-then
-    HYRAX_BUILD_VERSION="$HYRAX_BUILD_VERSION-test-deploy"
-    OLFS_BUILD_VERSION="$OLFS_BUILD_VERSION-test-deploy"
-fi
-
-
 export BES_SNAPSHOT_FILE=${BES_SNAPSHOT_FILE:-"./bes-snapshot"}
 if test -f "${BES_SNAPSHOT_FILE}"
 then
@@ -37,4 +20,33 @@ then
     # cat "${BES_SNAPSHOT_FILE}" | awk '{print "# "$0;}' >&2
 else
     echo "ERROR: Failed to located the bes snapshot file: ${BES_SNAPSHOT_FILE}" >&2
+    exit 2;
 fi
+
+td_tag_value="-test-deploy"
+test_deploy_tag=""
+if [[ "$TRAVIS_BRANCH" == *"$td_tag_value" ]]
+then
+    test_deploy_tag="$td_tag_value"
+fi
+if test -z "$test_deploy_tag"
+then
+    grep "test-deploy" "$BES_SNAPSHOT_FILE"
+    if $? -eq 0
+    then
+        test_deploy_tag="$td_tag_value"
+    fi
+fi
+echo "# test_deploy_tag: '${test_deploy_tag}'" >&2
+echo "#" >&2
+
+
+export OLFS_BUILD_NUMBER=$(expr $TRAVIS_BUILD_NUMBER - $TRAVIS_OLFS_BUILD_OFFSET)
+export OLFS_BUILD_VERSION="${OLFS_VERSION}-${OLFS_BUILD_NUMBER}${test_deploy_tag}"
+echo "#        OLFS_BUILD_VERSION: ${OLFS_BUILD_VERSION}" >&2
+
+export HYRAX_BUILD_NUMBER=$(expr $TRAVIS_BUILD_NUMBER - $TRAVIS_HYRAX_BUILD_OFFSET)
+export HYRAX_BUILD_VERSION="${HYRAX_VERSION}-${HYRAX_BUILD_NUMBER}${test_deploy_tag}"
+echo "#       HYRAX_BUILD_VERSION: ${HYRAX_BUILD_VERSION}" >&2
+echo "#" >&2
+
