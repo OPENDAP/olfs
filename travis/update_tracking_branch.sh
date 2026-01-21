273bb2814a8949044843e715b122d18ae8af8221
ndp-opendap <ndp@opendap.org>
2026-01-21 11:14:47 -0800

    wip

--- travis/update_tracking_branch.sh
+++ travis/update_tracking_branch.sh
@@ -23,28 +23,28 @@ loggy "Updating tracking branch..."
 loggy
 MAIN_BRANCH="${1:-"master"}"
 #MAIN_BRANCH="t1"
-loggy "MAIN_BRANCH: $MAIN_BRANCH"
+loggy "MAIN_BRANCH: '$MAIN_BRANCH'"
 
 TARGET_BRANCH="${2:-"tomcat-11"}"
 #TARGET_BRANCH="t2"
-loggy "TARGET_BRANCH: $TARGET_BRANCH"
+loggy "TARGET_BRANCH: '$TARGET_BRANCH'"
 
 loggy "Checking current branch..."
 current_branch="$(git branch --show-current)"
 check_status $?
-loggy "The current branch is: $current_branch"
+loggy "The current branch is: '$current_branch'"
 loggy
 
-loggy "Checking out branch: $TARGET_BRANCH"
+loggy "Checking out branch: '$TARGET_BRANCH'"
 git checkout "$TARGET_BRANCH"
 check_status $?
 
-loggy "Merging branch $MAIN_BRANCH into branch: $TARGET_BRANCH"
+loggy "Merging branch '$MAIN_BRANCH' into branch: '$TARGET_BRANCH'"
 git merge --verbose --no-edit "$MAIN_BRANCH"
 check_status $?
 
-loggy "Pushing changes for branch: $TARGET_BRANCH"
+loggy "Pushing changes for branch: '$TARGET_BRANCH'"
 git push
 check_status $?
 
-loggy "The branch $MAIN_BRANCH has been merged to the branch $TARGET_BRANCH  and the result has been pushed."
+loggy "The branch '$MAIN_BRANCH' has been merged to the branch '$TARGET_BRANCH'  and the result has been pushed."
