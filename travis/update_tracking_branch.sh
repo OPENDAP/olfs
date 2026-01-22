85675a8cdad02a21a23ea0da93f05e560eae9491
ndp-opendap <ndp@opendap.org>
2026-01-22 03:51:08 -0800

    Fixing issue from merge

--- travis/update_tracking_branch.sh
+++ travis/update_tracking_branch.sh
@@ -42,17 +42,14 @@ TRACKING_BRANCH="${2:-"tomcat-11"}"
 loggy "TARGET_BRANCH: '$TRACKING_BRANCH'"
 
 loggy "Fetching branches..."
-git fetch origin
+git fetch --all origin
 check_status $? "git fetch origin"
-
-loggy "Branches..."
-git branch -v -a
-check_status $? "git branch -v -a"
 
 loggy "Checking out branch: '$MAIN_BRANCH'"
 git checkout "$MAIN_BRANCH"
 check_status $? "git checkout $MAIN_BRANCH"
 
+loggy "Checking current branch..."
 current_branch="$(git branch --show-current)"
 check_status $? "git branch"
 loggy "The current branch is now: '$current_branch'"
