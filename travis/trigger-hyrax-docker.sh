eca0a7da23e3458933bfc212a3623e1c6eba6176
Nathan Potter <ndp@opendap.org>
2026-03-18 07:43:16 -0700

    Hyrax version refactor 2 (#286)
    * Refactored Hyrax and OLFS version determination.
    * Stop adding Hyrax version numbers to the OLFS code and drop them from the hyrax-docker build recippes

--- travis/trigger-hyrax-docker.sh
+++ travis/trigger-hyrax-docker.sh
@@ -42,12 +42,8 @@ loggy " TRAVIS_BUILD_RECIPE_FILE: $TRAVIS_BUILD_RECIPE_FILE"
 
 loggy "OLFS_BUILD_VERSION: $OLFS_BUILD_VERSION"
 
-OLFS_SNAPSHOT_TAG="olfs-${OLFS_BUILD_VERSION} "$(date "+%FT%T%z")"$test_deploy"
+OLFS_SNAPSHOT_TAG="olfs-${OLFS_BUILD_VERSION} $(date "+%FT%T%z")$test_deploy"
 loggy " OLFS_SNAPSHOT_TAG: $OLFS_SNAPSHOT_TAG"
-
-HYRAX_SNAPSHOT_TAG="hyrax-${HYRAX_BUILD_VERSION} "$(date "+%FT%T%z")"$test_deploy"
-loggy "HYRAX_SNAPSHOT_TAG: $HYRAX_SNAPSHOT_TAG"
-loggy ""
 
 loggy "Tagging olfs with: ${OLFS_BUILD_VERSION}"
 git tag -m "olfs-${OLFS_BUILD_VERSION}" -a "${OLFS_BUILD_VERSION}"
@@ -67,7 +63,6 @@ echo "TARGET_OS: $TARGET_OS" >  "$BUILD_RECIPE_FILE"
 echo "TOMCAT_MAJOR_VERSION: $TOMCAT_MAJOR_VERSION" >> "$BUILD_RECIPE_FILE"
 echo "${BES_SNAPSHOT}"       >> "$BUILD_RECIPE_FILE"
 echo "${OLFS_SNAPSHOT_TAG}"  >> "$BUILD_RECIPE_FILE"
-echo "${HYRAX_SNAPSHOT_TAG}" >> "$BUILD_RECIPE_FILE"
 loggy "Updated $BUILD_RECIPE_FILE file:"
 loggy "$(cat "$BUILD_RECIPE_FILE")"
 
@@ -86,19 +81,16 @@ Build Version Matrix:
 TARGET_OS: $TARGET_OS
 ${BES_SNAPSHOT}
 ${OLFS_SNAPSHOT_TAG}
-${HYRAX_SNAPSHOT_TAG}
 ";
 git status;
 
-export hyrax_tag="hyrax-${HYRAX_BUILD_VERSION}"
-loggy "Tagging hyrax-docker with: ${hyrax_tag}"
-git tag -m "${hyrax_tag}" -a "${hyrax_tag}"
+export olfs_tag="olfs-${OLFS_BUILD_VERSION}"
+loggy "Tagging hyrax-docker with: ${olfs_tag}"
+git tag -m "${olfs_tag}" -a "${olfs_tag}"
 
 
 loggy "Pushing to changes hyrax-docker:master:"
 git push "https://${GIT_UID}:${GIT_PSWD}@github.com/OPENDAP/hyrax-docker.git" --all
-loggy "Pushing to tags hyrax-docker:master:"
-git push "https://${GIT_UID}:${GIT_PSWD}@github.com/OPENDAP/hyrax-docker.git" "${hyrax_tag}"
 
 loggy "done"
 loggy "$HR"
