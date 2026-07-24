c20b119b62838aa7243f134299d140b4c1d2f5c2
Miguel Jimenez <mjimenez@opendap.org>
2026-07-24 12:02:08 -0700

    Release PR for olfs-1.19.0 (#312)
    * initial commit to release new version
    
    * Moved NEWS to NEWS.md
    
    * Update NEW with CVE
    
    * update build offset numbers
    
    * Numbers!
    
    ---------
    
    Co-authored-by: ndp-opendap <ndp@opendap.org>

--- travis/versions_and_build_offsets.sh
+++ travis/versions_and_build_offsets.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 #
-# When the release numbers are edited for a release, update these
+# When the release numbers are edited in configure.ac, update this
 # to the current Travis number so that the 'build number' in
 # x.y.z-<build number> is zero. jhrg 3/22/21
+#
+HR="=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+###########################################################################
+# loggy()
+function loggy() {
+  echo "$@" | awk '{ print "# travis_bes_build_offset.sh() - "$0;}' >&2
+}
 
-OLFS_VERSION=1.18.15
-TRAVIS_OLFS_BUILD_OFFSET=2188
-echo "#              OLFS_VERSION: ${OLFS_VERSION}" >&2
-echo "#  TRAVIS_OLFS_BUILD_OFFSET: ${TRAVIS_OLFS_BUILD_OFFSET}" >&2
+loggy "BEGIN $HR"
 
-HYRAX_VERSION=1.17.1
-TRAVIS_HYRAX_BUILD_OFFSET=2188
-echo "#             HYRAX_VERSION: ${HYRAX_VERSION}" >&2
-echo "# TRAVIS_HYRAX_BUILD_OFFSET: ${TRAVIS_HYRAX_BUILD_OFFSET}" >&2
+# This is the offical OLFS version, sans build number.
+export OLFS_VERSION="1.19.0"
+loggy "            OLFS_VERSION: ${OLFS_VERSION}" >&2
+
+# This is the TravisCI build number when the
+# last formal release was built.
+export TRAVIS_OLFS_BUILD_OFFSET=3256
+
+loggy "TRAVIS_OLFS_BUILD_OFFSET: ${TRAVIS_OLFS_BUILD_OFFSET}" >&2
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+  loggy "This is a Pull Request build for PR #$TRAVIS_PULL_REQUEST"
+  loggy "Setting TRAVIS_OLFS_BUILD_OFFSET to 0"
+  TRAVIS_OLFS_BUILD_OFFSET=0
+fi
+
+loggy "Using TRAVIS_OLFS_BUILD_OFFSET: $TRAVIS_OLFS_BUILD_OFFSET"
+loggy "END $HR"
